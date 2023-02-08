f7a9448b8c43ed5c1a80242503c6c77adb4453e6
md reyaj alam <mdreyajalal0007@gmil.com>
2023-02-08 22:03:15 +0530

    worked on songs funcitonality

--- src/app/models/api-endpoints.ts
+++ src/app/models/api-endpoints.ts
@@ -1,4 +1,8 @@
 export enum IApiEndpoints {
   LOGIN = 'auth/login',
-  REGISTER ="auth/register"
+  REGISTER ="auth/register",
+  POST_SONG ="auth/post-song",
+  UPDATE_SONG = "auth/update-song",
+  GET_SONG = "auth/get-song",
+  DELETE_SONG = "auth/delete-song",
 }
--- src/app/songs/songs.component.ts
+++ src/app/songs/songs.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { IApiEndpoints } from '../models/api-endpoints';
+import { IHitApi, RequestMethod } from '../models/global';
+import { GlobalService } from '../services/global.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-songs',
@@ -7,9 +11,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SongsComponent implements OnInit {
 
-  constructor() { }
+  songData :any;
+  songForm: FormGroup;
 
-  ngOnInit(): void {
+
+  constructor(private globalService: GlobalService) { 
+    this.songForm = new FormGroup({
+      songName: new FormControl('', [Validators.required]),
+    });
   }
 
+  ngOnInit(): void {
+  this.getSongData()
+  }
+
+  getSongData() {
+      const api: IHitApi = {
+        url: `${IApiEndpoints.GET_SONG}`,
+        requestMethod: RequestMethod.GET,
+        input: {},
+        response: (res: any) => {
+          if(res.status===200){
+            this.songData = res.result          
+          }else{
+            alert("No data found")
+          }
+        },
+        errorFunction: (error: any) => {
+          alert("Something went wrong")
+        },
+      };
+      this.globalService.hitApi(api);
+  }
+  onSubmit(value: any) {
+    if (this.songForm.valid) {
+      const api: IHitApi = {
+        url: `${IApiEndpoints.POST_SONG}`,
+        requestMethod: RequestMethod.POST,
+        input: {
+          songName: this.songForm.controls['songName'].value,
+        },
+        response: (res: any) => {
+          alert('Success');
+          this.songForm.reset({});
+          this.getSongData()
+        },
+        errorFunction: (error: any) => {
+          alert("Something went wrong")
+        },
+      };
+      this.globalService.hitApi(api);
+    } else {
+      alert('Invalid value');
+    }
+  }
 }
